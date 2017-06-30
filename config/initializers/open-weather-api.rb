fbc5cf992820d720b5a9de4665a40b308cf391f8
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2017-06-30 14:42:23 +0300

    install and configure open-weather-api gem

---
+++ config/initializers/open-weather-api.rb
@@ -0,0 +1,10 @@
+# Note that 'config' is an instance of `OpenWeatherAPI::API` (just name it as you like).
+OpenWeatherAPI.configure do |config|
+  # API key
+  config.api_key = "ece65e4c1820662d2a7b1c92104e1b60"
+
+  # Optionals
+  #config.default_language = 'es'     # 'en' by default
+  #config.default_country_code = 'es' # nil by default (ISO 3166-1 alfa2)
+  #config.default_units = 'metric'    # 'metric' by default
+end
